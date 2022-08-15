9bd40f3b3f888913c60ab169db5bcbd0c1795ce1
bsv120 <bsv@udm.ru>
2022-08-15 11:30:45 +0400

    first commit

---
+++ Tasks/base_restore.cmd
@@ -0,0 +1,1 @@
+cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 restore --db wms_test01 --bak-path c:\backup\wms_test01_orig11.bak --create-db --shrink-db --db-owner sa --db-recovery SIMPLE 
---
+++ Tasks/start_otgruzka_212.bat
@@ -0,0 +1,9 @@
+chcp 1251
+rem «апускаем цикл который должен повторитьс€ 
+rem „исло перемещаемых контейнеров задаетс€ в фиче
+
+for /l %%x in (1, 1, 60) do (
+	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=C:\Work\Vanessa\Projects\WMS_TEST\Tasks\VAParams_otgruzka_212.json"
+)
+
+
