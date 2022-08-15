9209fd176f5a54fcae901d452b5ab538f5754e4f
bsv120 <bsv@udm.ru>
2022-08-15 22:13:55 +0400

    Updates for git manage2

---
+++ start_priem_192.bat
@@ -0,0 +1,8 @@
+chcp 1251
+rem «апускаем цикл который должен повторитьс€ 60 раз
+
+for /l %%x in (1, 1, 60) do (
+	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_priem_192.json"
+)
+
+
