81fe99541dba7beabc36a6c8d994ee674857845d
bsv120 <bsv@udm.ru>
2022-08-31 14:52:09 +0400

    1

--- base_backup.bat
+++ base_backup.bat
@@ -1,3 +1,3 @@
 chcp 1251
-cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 backup --db wms_test01 --bak-path C:\BACKUP\wms_test01_orig17.bak
+cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 backup --db wms_test01 --bak-path C:\BACKUP\wms_test01_01092022.bak
 
--- base_restore.bat
+++ base_restore.bat
@@ -1,2 +1,2 @@
 chcp 1251
-cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 restore --db wms_test01 --bak-path c:\backup\wms_test01_orig16.bak --create-db --shrink-db --db-owner sa --db-recovery SIMPLE 
+cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 restore --db wms_test01 --bak-path c:\backup\wms_test01_clear_31082022.bak --create-db --shrink-db --db-owner sa --db-recovery SIMPLE 
