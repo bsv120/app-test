82186fb385a4fc35c286eb4ef637b10421ff677f
bsv120 <bsv@udm.ru>
2022-09-09 14:04:07 +0400

    change for pipe line use

--- scripts_public.bat
+++
@@ -1,5 +0,0 @@
-chcp 1251
-rem Публикуем на шару свеженькие скрипты и фичи.  Клиенты потом заберут
-
-copy C:\Work\Vanessa\Projects\WMS_TEST  \\sitec-fs01.sitec.corp\jenkins$ /Y
-
---
+++ start_otbor.bat
@@ -0,0 +1,12 @@
+chcp 1251
+rem Редактируем файл параметров запуска скрипта ванессы. Изменяем номер порта запуска и номера ТСД (совпадают)
+copy VAParams_otbor_orig.json VAParams_otbor.json
+nhrt.exe -spt:"000" -t:"%1" VAParams_otbor.json
+
+rem Запускаем цикл который должен повториться
+rem Число перемещаемых контейнеров задается в фиче
+
+for /l %%x in (1, 1, 60) do (
+	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_otbor.json"
+)
+
