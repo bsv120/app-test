0b0c82e12225e8fd9dabee1d976e61751052df9e
bsv120 <bsv@udm.ru>
2022-09-15 15:38:06 +0400

    1

--- base_backup.bat
+++ base_backup.bat
@@ -1,3 +1,3 @@
 chcp 1251
-cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 backup --db wms_test01 --bak-path C:\BACKUP\wms_test01_clear_12092022.bak
+cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 backup --db wms_test01 --bak-path C:\BACKUP\wms_test01_clear_16092022.bak
 
