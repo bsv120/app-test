924189404c7666fe8dfc0cc95ffe96f894460650
bsv120 <bsv@udm.ru>
2022-09-19 10:44:24 +0400

    1

--- start_priemka.bat
+++ start_priemka.bat
@@ -6,7 +6,7 @@ nhrt.exe -spt:"000" -t:"%1" VAParams_priemka.json
 rem «апускаем цикл который должен повторитьс€
 rem „исло перемещаемых контейнеров задаетс€ в фиче
 
-for /l %%x in (1, 1, 60) do (
+for /l %%x in (1, 1, 50) do (
 	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_priemka.json"
 )
 
