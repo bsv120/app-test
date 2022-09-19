5782088c205891be0750bb8641590dba3381659f
bsv120 <bsv@udm.ru>
2022-09-19 10:48:07 +0400

    1

--- base_backup.bat
+++
@@ -1,3 +0,0 @@
-chcp 1251
-cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 backup --db wms_test01 --bak-path C:\BACKUP\wms_test01_50_16092022.bak
-
--- base_restore.bat
+++
@@ -1,3 +0,0 @@
-chcp 1251
-rem Удалить в чистоя базе текущий сессии ТСД !!!
-cpdb database --srvr sitec-app01.sitec.corp --user sa --pwd assa@2022 restore --db wms_test01 --bak-path c:\backup\wms_test01_50_16092022.bak --create-db --shrink-db --db-owner sa --db-recovery SIMPLE 
