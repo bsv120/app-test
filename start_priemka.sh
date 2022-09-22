450c6c36eb6fa04c396cddd11fb91dbcad8f1fc2
bsv120 <bsv@udm.ru>
2022-09-22 11:14:56 +0400

    1

--- start_priemka.sh
+++ start_priemka.sh
@@ -1,8 +1,4 @@
-/opt/1cv8/x86_64/8.3.21.1393/1cv8c /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_priemka.json"
-/WS \"http://sitec-app01.sitec.corp/WMS_TEST01\"",
-
-
-/N"" /TestManager /Execute vanessa-automation-single.epf /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_priemka.json"
+/opt/1cv8/x86_64/8.3.21.1393/1cv8c /N""  /WS \"http://sitec-app01.sitec.corp/wms_testmanager\"" 
 
 
 
