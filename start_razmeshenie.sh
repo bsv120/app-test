b1c3ec68a5be14ca9a891356373f1f2aa64b3cfe
bsv120 <bsv@udm.ru>
2022-09-23 18:19:04 +0400

    add linux scripts

---
+++ start_razmeshenie.sh
@@ -0,0 +1,6 @@
+cp VAParams_orig.json VAParams.json
+
+sed -i "s/000/${1}/g" VAParams.json
+sed -i "s/Я ничего не делаю/Я размещаю контейнер/g" VAParams.json
+
+#/opt/1cv8/x86_64/8.3.21.1393/1cv8c /TestManager "/S sitec-app01.sitec.corp\wms_testmanager" /Execute vanessa-automation-single.epf ";" /C "StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams.json"
