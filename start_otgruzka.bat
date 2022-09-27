19906d23ed3065c68a2763dd3974c1211b64e0c0
bsv120 <bsv@udm.ru>
2022-09-27 09:32:34 +0400

    add linux scripts2

--- start_otgruzka.bat
+++ start_otgruzka.bat
@@ -10,6 +10,6 @@ rem «апускаем цикл который должен повторитьс€
 rem „исло перемещаемых контейнеров задаетс€ в фиче
 
 for /l %%x in (1, 1, 50) do (
-	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" /IBConnectionString "File=""C:\Work\WMS"";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams_otgruzka.json"
+	"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager "/S sitec-app01.sitec.corp\wms_testmanager" /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" ";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams.json"
 )
 
