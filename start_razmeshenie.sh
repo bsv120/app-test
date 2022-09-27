bd9b2cdceb804748572a7af2822b972d2c4bd024
bsv120 <bsv@udm.ru>
2022-09-27 10:51:11 +0400

    add linux scripts4

--- start_razmeshenie.sh
+++ start_razmeshenie.sh
@@ -4,4 +4,7 @@ sed -i "s/000/${1}/g" VAParams.json
 sed -i "s/Я ничего не делаю/Я размещаю контейнер/g" VAParams.json
 sed -i "s/Путь до эмулятора/\/home\/bsv\/Work\/WMS\/ЭмуляторТСД_Тест02.epf\\\/g" VAParams.json
 
+for (( i=1; i <= 50; i++ ))
+do
 /opt/1cv8/x86_64/8.3.21.1393/1cv8c /TestManager "/S sitec-app01.sitec.corp\wms_testmanager" /Execute vanessa-automation-single.epf ";" /C "StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams.json"
+done
