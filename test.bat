548a3c5ea2213e798621b14d193c71fc342f572c
bsv120 <bsv@udm.ru>
2022-10-10 14:59:15 +0400

    add zameru 2

---
+++ test.bat
@@ -0,0 +1,12 @@
+chcp 1251
+copy VAParams_orig.json VAParams.json
+
+nhrt.exe -spt:"000" -t:"%1" VAParams.json
+nhrt.exe -spt:"Я ничего не делаю" -t:"Я размещаю контейнер" VAParams.json
+nhrt.exe -spt:"Путь до эмулятора" -t:"C:\\Work\\WMS\\ЭмуляторТСД_Тест04.epf\" VAParams.json
+
+rem Запускаем цикл который должен повториться
+rem Число перемещаемых контейнеров задается в фиче
+
+"C:\Program Files\1cv8\8.3.21.1393\bin\1cv8c" /N"" /TestManager "/WS http://sitec-app01.sitec.corp/wms_testmanager" /Execute "C:\Work\Vanessa\vanessa-automation-single\vanessa-automation-single.epf" ";" /C"StartFeaturePlayer;QuietInstallVanessaExt;VAParams=VAParams.json"
+
